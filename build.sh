15f156635073c7f854892999bd4e168ea42fb1ad
Peter Palaga <ppalaga@redhat.com>
2016-09-15 09:43:57 +0200

    Extended the reproducer to be runnable on EAP7 too.

--- build.sh
+++ build.sh
@@ -8,6 +8,14 @@ if [ "x$JBOSS_HOME" == "x" ]; then
   exit 1
 fi
 
+for param in "$@" ; do
+    case $param in
+        -eap6)      eapMajor="6" ;;
+        -eap7)      eapMajor="7" ;;
+    esac
+done
+
+
 rm -Rf $JBOSS_HOME/standalone/deployments/*
 
 rm -Rf $JBOSS_HOME/standalone/log/*
@@ -20,6 +28,6 @@ i=2
 mvn clean install -Dapp-index=${i}
 cp -t $JBOSS_HOME/standalone/deployments target/bz1374457-${i}.war
 
-cp -t $JBOSS_HOME/standalone/configuration standalone.xml
+cp standalone-eap-${eapMajor}.xml $JBOSS_HOME/standalone/configuration/standalone.xml
 
 $JBOSS_HOME/bin/standalone.sh
